fa9001f2206f062cb5c6dce1c3399f62cf226610
yacouty <102199921+yacouty@users.noreply.github.com>
2022-04-06 18:00:27 +0200

    Add files via upload

--- main.py
+++ main.py
@@ -1,187 +1,51 @@
-#refrence https://scikit-image.org/docs/stable/api/skimage.util.html#random-noise
-import cv2
+from cv2 import cv2
 import numpy as np
-#from .dtype import img_as_float
-from skimage.util import img_as_float
-from skimage.metrics import structural_similarity as ssim
 import math
 import matplotlib.pyplot as plt
 
-__all__ = ['random_noise']
-img = cv2.imread('DSCN0479-001.jpg',
-                 cv2.IMREAD_GRAYSCALE)
-
-def _bernoulli(p, shape, *, random_state):
-    """
-    Bernoulli trials at a given probability of a given size.
-    This function is meant as a lower-memory alternative to calls such as
-    `np.random.choice([True, False], size=image.shape, p=[p, 1-p])`.
-    While `np.random.choice` can handle many classes, for the 2-class case
-    (Bernoulli trials), this function is much more efficient.
-    Parameters
-    ----------
-    p : float
-        The probability that any given trial returns `True`.
-    shape : int or tuple of ints
-        The shape of the ndarray to return.
-    seed : `numpy.random.Generator`
-        ``Generator`` instance.
-    Returns
-    -------
-    out : ndarray[bool]
-        The results of Bernoulli trials in the given `size` where success
-        occurs with probability `p`.
-    """
-    if p == 0:
-        return np.zeros(shape, dtype=bool)
-    if p == 1:
-        return np.ones(shape, dtype=bool)
-    return random_state.random(shape) <= p
+jetplane=cv2.imread("jetplane.tif")
+jetplane=cv2.resize(jetplane,(800,600))
+s=jetplane.shape
+jetplaneGray=cv2.cvtColor(jetplane,cv2.COLOR_BGR2GRAY)
+cv2.imshow('binary',jetplaneGray)
 
 
-def random_noise(image, mode='gaussian', seed=None, clip=True, **kwargs):
-    """
-    Function to add random noise of various types to a floating-point image.
-    Parameters
-    ----------
-    image : ndarray
-        Input image data. Will be converted to float.
-    mode : str, optional
-        One of the following strings, selecting the type of noise to add:
-        - 'gaussian'  Gaussian-distributed additive noise.
-        - 'localvar'  Gaussian-distributed additive noise, with specified
-                      local variance at each point of `image`.
-        - 'poisson'   Poisson-distributed noise generated from the data.
-        - 'salt'      Replaces random pixels with 1.
-        - 'pepper'    Replaces random pixels with 0 (for unsigned images) or
-                      -1 (for signed images).
-        - 's&p'       Replaces random pixels with either 1 or `low_val`, where
-                      `low_val` is 0 for unsigned images or -1 for signed
-                      images.
-        - 'speckle'   Multiplicative noise using out = image + n*image, where
-                      n is Gaussian noise with specified mean & variance.
-    seed : {None, int, `numpy.random.Generator`}, optional
-        If `seed` is None the `numpy.random.Generator` singleton is
-        used.
-        If `seed` is an int, a new ``Generator`` instance is used,
-        seeded with `seed`.
-        If `seed` is already a ``Generator`` instance then that
-        instance is used.
-        This will set the random seed before generating noise,
-        for valid pseudo-random comparisons.
-    clip : bool, optional
-        If True (default), the output will be clipped after noise applied
-        for modes `'speckle'`, `'poisson'`, and `'gaussian'`. This is
-        needed to maintain the proper image data range. If False, clipping
-        is not applied, and the output may extend beyond the range [-1, 1].
-    mean : float, optional
-        Mean of random distribution. Used in 'gaussian' and 'speckle'.
-        Default : 0.
-    var : float, optional
-        Variance of random distribution. Used in 'gaussian' and 'speckle'.
-        Note: variance = (standard deviation) ** 2. Default : 0.01
-    local_vars : ndarray, optional
-        Array of positive floats, same shape as `image`, defining the local
-        variance at every image point. Used in 'localvar'.
-    amount : float, optional
-        Proportion of image pixels to replace with noise on range [0, 1].
-        Used in 'salt', 'pepper', and 'salt & pepper'. Default : 0.05
-    salt_vs_pepper : float, optional
-        Proportion of salt vs. pepper noise for 's&p' on range [0, 1].
-        Higher values represent more salt. Default : 0.5 (equal amounts)
-    Returns
-    -------
-    out : ndarray
-        Output floating-point image data on range [0, 1] or [-1, 1] if the
-        input `image` was unsigned or signed, respectively.
-    Notes
-    -----
-    Speckle, Poisson, Localvar, and Gaussian noise may generate noise outside
-    the valid image range. The default is to clip (not alias) these values,
-    but they may be preserved by setting `clip=False`. Note that in this case
-    the output may contain values outside the ranges [0, 1] or [-1, 1].
-    Use this option with care.
-    Because of the prevalence of exclusively positive floating-point images in
-    intermediate calculations, it is not possible to intuit if an input is
-    signed based on dtype alone. Instead, negative values are explicitly
-    searched for. Only if found does this function assume signed input.
-    Unexpected results only occur in rare, poorly exposes cases (e.g. if all
-    values are above 50 percent gray in a signed `image`). In this event,
-    manually scaling the input to the positive domain will solve the problem.
-    The Poisson distribution is only defined for positive integers. To apply
-    this noise type, the number of unique values in the image is found and
-    the next round power of two is used to scale up the floating-point result,
-    after which it is scaled back down to the floating-point image range.
-    To generate Poisson noise against a signed image, the signed image is
-    temporarily converted to an unsigned image in the floating point domain,
-    Poisson noise is generated, then it is returned to the original range.
-    """
-    mode = mode.lower()
+def Hist(image):
+    H=np.zeros(shape=(256,1))
+    s=image.shape
+    for i in range(s[0]):
+        for j in range(s[1]):
+            k=image[i,j]
+            H[k,0]=H[k,0]+1
+    return H
 
-    # Detect if a signed image was input
-    if image.min() < 0:
-        low_clip = -1.
-    else:
-        low_clip = 0.
+histg=Hist(jetplaneGray)
+plt.plot(histg)
+def hist_eq(img,bins):
+    x=histg.reshape(1,256)
+    y=np.array([])
+    y=np.append(y,x[0,0])
 
-    image = img_as_float(image)
+    for i in range(255):
+        k=x[0,i+1]+y[i]
+        y=np.append(y,k)
+    y=np.round((y/(s[0]*s[1]))*(bins-1))
 
-    rng = np.random.default_rng(seed)
+    for i in range(s[0]):
+        for j in range(s[1]):
+            k=img[i,j]
+            img[i,j]=y[k]
 
-    allowedtypes = {
-        'gaussian': 'gaussian_values',
-        'localvar': 'localvar_values',
-        'poisson': 'poisson_values',
-        'salt': 'sp_values',
-        'pepper': 'sp_values',
-        's&p': 's&p_values',
-        'speckle': 'gaussian_values'}
 
-    kwdefaults = {
-        'mean': 0.,
-        'var': 0.05,
-        'amount': 0.05,
-        'salt_vs_pepper': 0.5,
-        'local_vars': np.zeros_like(image) + 0.01}
-
-    allowedkwargs = {
-        'gaussian_values': ['mean', 'var'],
-        'localvar_values': ['local_vars'],
-        'sp_values': ['amount'],
-        's&p_values': ['amount', 'salt_vs_pepper'],
-        'poisson_values': []}
-
-    for key in kwargs:
-        if key not in allowedkwargs[allowedtypes[mode]]:
-            raise ValueError('%s keyword not in allowed keywords %s' %
-                             (key, allowedkwargs[allowedtypes[mode]]))
-
-    # Set kwarg defaults
-    for kw in allowedkwargs[allowedtypes[mode]]:
-        kwargs.setdefault(kw, kwdefaults[kw])
-
-    if mode == 'gaussian':
-        noise = rng.normal(kwargs['mean'], kwargs['var'] ** 0.5, image.shape)
-        out = image + noise
-    elif mode == 's&p':
-        out = image.copy()
-        p = kwargs['amount']
-        q = kwargs['salt_vs_pepper']
-        flipped = _bernoulli(p, image.shape, random_state=rng)
-        salted = _bernoulli(q, image.shape, random_state=rng)
-        peppered = ~salted
-        out[flipped & salted] = 1
-        out[flipped & peppered] = low_clip
-
-    return out
-a=random_noise(img, mode='gaussian', seed=None, clip=True)
-cv2.imshow('gaussianNoise',a)
-cv2.imwrite('GaussianNoise.jpg',255*a)
+hist_eq(jetplaneGray,64)
+equal=Hist(jetplaneGray)
+plt.plot(histg)
+plt.figure()
+plt.plot(equal)
+plt.show()
+cv2.imshow("equalized",jetplaneGray)
 cv2.waitKey(0)
 
-noise_img = random_noise(img, mode='s&p',amount=0.05)
-cv2.imshow('S&P',noise_img)
-cv2.imwrite('S&P.jpg',255*noise_img)
-cv2.waitKey(0)
+## reference https://youtu.be/cVg2WiAX8Lg
 
 
